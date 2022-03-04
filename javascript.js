cbc19e10a45caa3b397c71b7c2add3104b2dcfff
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2022-03-04 13:17:50 -1000

    Expand playerPlay() to check for valid input

--- javascript.js
+++ javascript.js
@@ -14,17 +14,32 @@ function computerPlay(){
 };
 
 function playerPlay(){
-  let input = prompt("Rock, paper, or scissors?");
-  return input.toLowerCase();
+  let input = prompt("Rock, paper, or scissors?").toLowerCase();
+  if (input === "rock" || input === "paper" || input === "scissors")
+    return input;
+  else{
+    while(input != "rock" || input != "paper" || input != "scissors")
+      input = prompt("It's gotta be rock, paper, or scissors, dude.").toLowerCase();
+    return input;
+  }
 };
 
 function playRound(playerSelection, computerSelection){
   if (playerSelection === computerSelection){
-    return "Draw!";
+    console.log("Draw!");
+    return 0;
   }
   else{
     return "Not draw!";
   }
+};
+
+function declareRoundWin{
+  console.log("You won this round!");
+};
+
+function declareRoundLose{
+  console.log("You lost this round!");
 };
 
 function testCompPlay(){
@@ -39,6 +54,10 @@ function testCompPlay(){
   console.log(computerPlay());
   console.log(computerPlay());
 };
+
+function game(){
+  
+}
 
 console.log(playRound(playerPlay(), computerPlay()));
 // testCompPlay();
