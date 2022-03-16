25388ff11228c3f74366b799b5ecc4d8825d2244
GophTheGreat <13459026+GophTheGreat@users.noreply.github.com>
2022-03-15 15:08:50 -1000

    Removed autoplay

--- javascript.js
+++ javascript.js
@@ -14,17 +14,73 @@ function computerPlay(){
 };
 
 function playerPlay(){
-  let input = prompt("Rock, paper, or scissors?");
-  return input.toLowerCase();
+  let input = prompt("Rock, paper, or scissors?").toLowerCase();
+  if (input === "rock" || input === "paper" || input === "scissors")
+    return input;
+  else{
+    while(input != "rock" && input != "paper" && input != "scissors")
+      input = prompt("It's gotta be rock, paper, or scissors, dude.").toLowerCase();
+    return input;
+  }
 };
 
 function playRound(playerSelection, computerSelection){
+  console.log(`You played ${playerSelection}! Computer played ${computerSelection}!`);
   if (playerSelection === computerSelection){
-    return "Draw!";
+    console.log("Draw!");
+    return 0;
+  }
+  else if(playerSelection === "rock")
+  {
+    if (computerSelection === "paper")
+    {
+      declareRoundLose();
+      return 0;
+    }
+    if (computerSelection === "scissors")
+    {
+      declareRoundWin();
+      return 1;
+    }
+  }
+  else if(playerSelection === "paper")
+  {
+    if (computerSelection === "rock")
+    {
+      declareRoundWin();
+      return 1;
+    }
+    if (computerSelection === "scissors")
+    {
+      declareRoundLose();
+      return 0;
+    }
+  }
+  else if(playerSelection === "scissors")
+  {
+    if (computerSelection === "paper")
+    {
+      declareRoundWin();
+      return 1;
+    }
+    if (computerSelection === "rock")
+    {
+      declareRoundLose();
+      return 0;
+    }
   }
   else{
-    return "Not draw!";
+    console.log("This should never fire D:");
+    return 0;
   }
+}
+
+function declareRoundWin(){
+  console.log("You won this round!");
+};
+
+function declareRoundLose(){
+  console.log("You lost this round!");
 };
 
 function testCompPlay(){
@@ -40,5 +96,15 @@ function testCompPlay(){
   console.log(computerPlay());
 };
 
-console.log(playRound(playerPlay(), computerPlay()));
+function game(){
+  let playCount = 0;
+  let winCount = 0;
+  for (playCount; playCount < 5; ++playCount){
+    winCount += playRound(playerPlay(), computerPlay()); 
+  }
+  console.log(`You won ${winCount} times!`)
+}
+
+//console.log(playRound(playerPlay(), computerPlay()));
+//game();
 // testCompPlay();
