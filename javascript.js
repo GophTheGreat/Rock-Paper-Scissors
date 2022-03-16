8bdeb91abb32143fd49661df36162ba40d0e30c6
Goph <tealeverywhere@gmail.com>
2022-03-15 20:10:45 -1000

    push javascript pls

--- javascript.js
+++ javascript.js
@@ -45,28 +45,16 @@ function playRound(playerSelection, computerSelection){
   console.log(`You played ${playerSelection}! Computer played ${computerSelection}!`);
   if (playerSelection === computerSelection){
     console.log("Draw!");
-<<<<<<< HEAD
-    return 0;
-=======
->>>>>>> rps-ui
   }
   else if(playerSelection === "rock")
   {
     if (computerSelection === "paper")
     {
       declareRoundLose();
-<<<<<<< HEAD
-      return 0;
-=======
->>>>>>> rps-ui
     }
     if (computerSelection === "scissors")
     {
       declareRoundWin();
-<<<<<<< HEAD
-      return 1;
-=======
->>>>>>> rps-ui
     }
   }
   else if(playerSelection === "paper")
@@ -74,18 +62,10 @@ function playRound(playerSelection, computerSelection){
     if (computerSelection === "rock")
     {
       declareRoundWin();
-<<<<<<< HEAD
-      return 1;
-=======
->>>>>>> rps-ui
     }
     if (computerSelection === "scissors")
     {
       declareRoundLose();
-<<<<<<< HEAD
-      return 0;
-=======
->>>>>>> rps-ui
     }
   }
   else if(playerSelection === "scissors")
@@ -93,26 +73,14 @@ function playRound(playerSelection, computerSelection){
     if (computerSelection === "paper")
     {
       declareRoundWin();
-<<<<<<< HEAD
-      return 1;
-=======
->>>>>>> rps-ui
     }
     if (computerSelection === "rock")
     {
       declareRoundLose();
-<<<<<<< HEAD
-      return 0;
-=======
->>>>>>> rps-ui
     }
   }
   else{
     console.log("This should never fire D:");
-<<<<<<< HEAD
-    return 0;
-  }
-=======
     return;
   }
   playCount++;
@@ -139,23 +107,16 @@ function playRound(playerSelection, computerSelection){
 
 
   
->>>>>>> rps-ui
 }
 
 function declareRoundWin(){
   console.log("You won this round!");
-<<<<<<< HEAD
-=======
   winCount++;
->>>>>>> rps-ui
 };
 
 function declareRoundLose(){
   console.log("You lost this round!");
-<<<<<<< HEAD
-=======
   computerWinCount++;
->>>>>>> rps-ui
 };
 
 function testCompPlay(){
@@ -171,28 +132,7 @@ function testCompPlay(){
   console.log(computerPlay());
 };
 
-<<<<<<< HEAD
-function game(){
-  let playCount = 0;
-  let winCount = 0;
-  for (playCount; playCount < 5; ++playCount){
-    winCount += playRound(playerPlay(), computerPlay()); 
-  }
-  console.log(`You won ${winCount} times!`)
-}
 
-//console.log(playRound(playerPlay(), computerPlay()));
-game();
-=======
-
-function game(){
-  while(winCount == 5 || computerWinCount == 5)
-  {
-    displayWins.textContent = 
-    `Game over! Play moare?!`
-  }
-
-}
 
 
 buttons.forEach(button => button.addEventListener("click", () => {
@@ -202,5 +142,4 @@ buttons.forEach(button => button.addEventListener("click", () => {
 //console.log(playRound(playerPlay(), computerPlay()));
 
 
->>>>>>> rps-ui
 // testCompPlay();
